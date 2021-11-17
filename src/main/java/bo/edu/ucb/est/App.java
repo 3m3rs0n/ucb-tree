1482c5099442f2a832e007ab723d2f4077b6cf95
3m3rs0n <emersonchipana12345@gmail.com>
2021-11-17 13:43:35 -0400

    Agregando remove con todos los casos y printInOrderNonRecursive

--- src/main/java/bo/edu/ucb/est/App.java
+++ src/main/java/bo/edu/ucb/est/App.java
@@ -1,5 +1,7 @@
 package bo.edu.ucb.est;
 
+import java.util.Stack;
+
 /**
  * Hello world!
  *
@@ -8,19 +10,49 @@ public class App
 {
     public static void main( String[] args )  {
         Tree<Integer> tree = new Tree<Integer>();
-        tree.add(5);
-        tree.add(4);
-        tree.add(7);
-        tree.add(6);
+        tree.add2(15);
+        tree.add2(9);
+        tree.add2(16);
+        tree.add2(2);
+        tree.add2(1);
+        tree.add2(5);
+        tree.add2(12);
+        tree.add2(11);
+        tree.add2(14);
+        tree.add2(16);
+        tree.add2(20);
+        tree.add2(19);
+        tree.add2(22);
+
         System.out.println("========InOrder=========");
-        Tree.printInOrder(tree.getRoot());
+        Tree.printInOrderNonRecursive(tree.getRoot());
         System.out.println("FIN");
+
+        tree.remove(9);
+
+        System.out.println("========InOrder=========");
+        Tree.printInOrderNonRecursive(tree.getRoot());
+        System.out.println("FIN");
+
         System.out.println("======PreOrder=========");
         Tree.printPreOrder(tree.getRoot());
         System.out.println("FIN");
         System.out.println("======PostOrder=========");
         Tree.printPostOrder(tree.getRoot());
         System.out.println("FIN");
+
+        Stack<String> stringStack = new Stack<>();
+        stringStack.push("Alfa");
+        stringStack.push("Beta");
+        stringStack.push("Bravo");
+        stringStack.push("Gama");
+
+        System.out.println("Contenido de la pila:" + stringStack);
+        System.out.println("Primer pop:" + stringStack.pop());
+        System.out.println("Segundo pop:" + stringStack.pop());
+        System.out.println("Primer peek:" + stringStack.peek());
+        System.out.println("Primer peek:" + stringStack.peek());
+
 
     }
 }
--- src/main/java/bo/edu/ucb/est/Tree.java
+++ src/main/java/bo/edu/ucb/est/Tree.java
@@ -1,7 +1,10 @@
 package bo.edu.ucb.est;
 
+import java.util.Stack;
+
 public class Tree<D extends Comparable<D>> {
     private Node<D> root;
+
 
     public Tree() {
     }
@@ -43,6 +46,122 @@ public class Tree<D extends Comparable<D>> {
             }
         }
     }
+
+
+
+        /**
+     * 1. Crear un Stack vacio S.
+     * 2. Inicializar el nodo current como root.
+     * 3. Insertar el nodo current a S y asignar current = current.getLeft(), hasta que el actual sea NULL.
+     * 4. Si current es NULL y S no esta vacio, entonces:
+     *      4.1) Hacemos pop de S.
+     *      4.2) Imprimos el elemento que hicimos pop y asignamos current = pop_node.getRight();
+     *      4.3) Ir al paso 3.
+     * 5. Si current es NULL, y S esta vacio entocnes hemos terminado.
+     * @param root nodo a partir del cual ser comienza a realizar in order recursiva
+     */
+    public static void printInOrderNonRecursive(Node<?> root) {
+        Stack<Node<?>> S = new Stack<>();
+        Node<?> current = root;
+        
+        while(current!=null){
+            S.push(current);
+            current = current.getLeft();
+        } 
+        while (!S.isEmpty()){
+            current=S.pop();
+            System.out.print(current.getData()+"->");
+            if(current.getRight()!=null){
+                current = current.getRight();
+                while(current!=null){
+                    S.push(current);
+                    current = current.getLeft();
+                }
+            }
+        }
+        
+
+    }
+
+    /**
+     * 1. El nodo que se va a eliminar no tiene hijo, es una hoja.
+     * Este es el caso más simple; dado que un nodo hoja no tiene hijos, no necesitamos preocuparnos por nada. Podemos reemplazar el nodo hoja con NULL y liberar el espacio asignado a este nodo.
+     *
+     * 2. El nodo que se va a eliminar tiene un solo hijo (hijo izquierdo o derecho).
+     * En este caso, almacenamos el hijo del nodo y eliminamos el nodo de su posición original. Luego, el nodo hijo se inserta en la posición original del nodo eliminado.
+     *
+     * 3. El nodo que se va a eliminar tiene hijos, hijo izquierdo y derecho.
+     * Este es el caso más complicado porque aquí, no podemos simplemente eliminar o reemplazar el nodo con su hijo. En este caso, encontramos el nodo más pequeño en el subárbol derecho del nodo minnode. Reemplace el valor del nodo que se eliminará con el valor de minnode y llame de forma recursiva a delete en este nodo.
+     * @param data
+     */
+    public void remove(D data) {
+        Node<D> nodoAEliminar = null;
+        Node<D> nodoAnterior = null;
+        for( Node<D> current=root; current!=null ;){ //recorrido
+            if(current.getData().compareTo(data)>0){
+                nodoAnterior = current;
+                current = current.getLeft();
+            } else if(current.getData().compareTo(data)<0){
+                nodoAnterior = current;
+                current = current.getRight();
+            } else{
+                nodoAEliminar = current;
+                break;
+            }
+        }
+        if(nodoAEliminar.getRight()== null && nodoAEliminar.getLeft()== null){ //caso 1. El nodo a eliminar es una hoja
+            if(nodoAnterior.getLeft()==nodoAEliminar){
+                nodoAnterior.setLeft(null);
+            }else{
+                nodoAnterior.setRight(null);
+            }
+        }else{//caso 3. El nodo a eliminar tiene dos hijos
+            if(nodoAEliminar.getRight()!=null && nodoAEliminar.getLeft()!=null){
+                Node<D> minNode = null;
+                for( Node<D> current=nodoAEliminar.getRight(); current!=null ;){//Recorremos hasta dar con el minNode
+                    minNode = current;
+                    current = current.getLeft();
+                }
+                System.out.println(minNode.getData());
+                remove(minNode.getData());
+                minNode.setRight(nodoAEliminar.getRight());
+                minNode.setLeft(nodoAEliminar.getLeft());
+                if(nodoAnterior.getLeft()==nodoAEliminar){
+                    nodoAnterior.setLeft(minNode);
+                }else{
+                    nodoAnterior.setRight(minNode);
+                }
+            }else{//caso 2. Cuando tiene solo un hijo
+                if(nodoAnterior.getLeft()==nodoAEliminar){
+                    if(nodoAEliminar.getRight()== null && nodoAEliminar.getLeft()!= null){
+                        nodoAnterior.setLeft(nodoAEliminar.getLeft());
+                    }else if(nodoAEliminar.getRight()!= null && nodoAEliminar.getLeft()== null){
+                        nodoAnterior.setLeft(nodoAEliminar.getRight());
+                    }
+                }else{
+                    if(nodoAEliminar.getRight()== null && nodoAEliminar.getLeft()!= null){
+                        nodoAnterior.setRight(nodoAEliminar.getLeft());
+                    }else if(nodoAEliminar.getRight()!= null && nodoAEliminar.getLeft()== null){
+                        nodoAnterior.setRight(nodoAEliminar.getRight());
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //        Hasta que todos los nodos hayan sido atravesados −
 //        Paso 1 − Recorre recursivamente el subarbol izquierdo.
@@ -89,25 +208,25 @@ public class Tree<D extends Comparable<D>> {
         if ( root == null) { // arbol vacio
             root = newNode;
         }else{
-            for( Node<D> current=root; current!=null;){
-                if(current.getData().compareTo(data)>0){
-                    if(current.getLeft()==null){
-                        current.setLeft(newNode);
-                        break;
-                    }else{
-                        current = current.getLeft();
-                    }
-                }else if(current.getData().compareTo(data) < 0){
-                    if(current.getRight()==null){
-                        current.setRight(newNode);
-                        break;
-                    }else{
-                        current = current.getRight();
-                    }
-                }else{
-                    break;
-                }
+            addDataToNode(root,newNode);
+        }
+    }
+
+    public void addDataToNode(Node<D> current, Node<D> newNode){
+        if (current.getData().compareTo(newNode.getData()) > 0){
+            if (current.getLeft()==null){
+                current.setLeft(newNode);
+            }else{
+                addDataToNode(current.getLeft(), newNode);
+            }
+        }else if (current.getData().compareTo(newNode.getData()) < 0){
+            if (current.getRight()==null){
+                current.setRight(newNode);
+            }else{
+                addDataToNode(current.getRight(), newNode);
             }
         }
+        
+
     }
 }
